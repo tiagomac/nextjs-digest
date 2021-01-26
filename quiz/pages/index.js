2bc2aac79e05726d5379854049272b6d81550159
Tiago Mesquita <tiagomac@gmail.com>
2021-01-26 14:27:14 -0300

    inclusão de nova página

--- quiz/pages/index.js
+++ quiz/pages/index.js
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -18,29 +21,54 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-    return (
-        <QuizBackground backgroundImage={db.bg}>
-            <QuizContainer>
-                <QuizLogo/>
-                <Widget>
-                    <Widget.Header>
-                        <h1>{db.title}</h1>
-                    </Widget.Header>
-                    <Widget.Content>
-                        <p>{db.description}</p>
-                    </Widget.Content>
-                </Widget>
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
-                <Widget>
-                    <Widget.Content>
-                        <h1>Quizes da Galera</h1>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                  onChange={function (infosDoEvento) {
+                    console.log(infosDoEvento.target.value);
+                    // State
+                    // name = infosDoEvento.target.value;
+                    setName(infosDoEvento.target.value);
+                  }}
+                  placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
 
-                        <p>lorem ipsum dolor sit amet...</p>
-                    </Widget.Content>
-                </Widget>
-                <Footer/>
-            </QuizContainer>
-            <GitHubCorner projectUrl="https://github.com/tiagomac"/>
-        </QuizBackground>
-    );
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/tiagomac" />
+    </QuizBackground>
+  );
 }
---
+++ quiz/pages/quiz.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Quiz = styled.div`
+  color: chocolate;
+  font-size: 6em;
+`
+
+export default function PudimComBatata() {
+    return (
+        <Quiz>
+            Página de quiz
+        </Quiz>
+    );
+}
