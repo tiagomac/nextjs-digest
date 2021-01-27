99c51d7ce9303cbc3cb1b2750643904c03e28d16
Tiago Mesquita <tiagomac@gmail.com>
2021-01-27 20:21:54 -0300

    novos ajustes e inclusão de componentes Quiz

--- quiz/pages/index.js
+++ quiz/pages/index.js
@@ -8,6 +8,8 @@ import GitHubCorner from '../src/components/GitHubCorner';
 import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -27,34 +29,31 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>AluraQuiz - Modelo Base</title>
+        <title>{db.title}</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>The legend of zelda</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
+            <p>{db.description}</p>
             <form onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
               console.log('Fazendo uma submissão por meio do react');
             }}
             >
-              <input
-                  onChange={function (infosDoEvento) {
-                    console.log(infosDoEvento.target.value);
-                    // State
-                    // name = infosDoEvento.target.value;
-                    setName(infosDoEvento.target.value);
-                  }}
+              <Input
+                  name="nomeDoUsuario"
+                  onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                   placeholder="Diz ai seu nome"
+                  value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
